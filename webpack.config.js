a9815dd1ffc6443342998c5c6a1e3fb3f20029d0
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-08-05 13:05:30 +0100

    Add and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/restaurant.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/restaurant.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
