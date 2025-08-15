190ccb2f05e14720d727372bfb9bbeccf9a0f12d
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2025-08-15 11:09:43 +0100

    add logic for tabbed content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import "./styles.css";
 import {mainPage} from "./mainPage.js";
 
-mainPage();
+const content = document.querySelector("#content");
 
-console.log("restaurant");
+const mainBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector("#about");
+
+function listen() {
+    mainBtn.addEventListener("click", function() {
+        clearContent();
+        mainPage();
+        alert("main");
+    });
+    menuBtn.addEventListener("click", function() {
+        clearContent();
+        alert("menu");
+        //menuPage();
+    });
+    aboutBtn.addEventListener("click", function() {
+        clearContent();
+        alert("about");
+        //aboutPage();
+    })
+}
+
+function clearContent() {
+    content.innerHTML = "";
+}
+
+mainPage();
+listen();
